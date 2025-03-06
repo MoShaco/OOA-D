07c8c4d9d1ea095690a4484a233bf1d02ee18739
Mohammed Shaco <mohammedshaco@gmail.com>
2025-03-06 11:19:36 +0200

    test alternative path, when the door closes and the dog outside

--- second/dog_door_simulator.py
+++ second/dog_door_simulator.py
@@ -1,21 +1,24 @@
 from dog_door import DogDoor
 from remote import Remote
-
+import time
 
 def main():
     door = DogDoor()
     remote = Remote(door=door)
-
-    print("Fido barks to go outside...")
     remote.press_button()
 
     print("Fido has gone outside... ")
-
     print("Fido's all done...")
+
+    time.sleep(10)
+
+    print("... but he's stuck outside!")
+    print("\n Fido starts barking ...")
+    print("... so Gina grabs the remote control.")
+
+    remote.press_button()
     
-    print("Fido's back inside... ")
-
-
+    print("\n Fido's back inside ...")
 
 if __name__ == "__main__":
     main()
