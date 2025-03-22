3a9db58cd39d0d31251db8de80d0280857cde2ed
Mohammed Shaco <mohammedshaco@gmail.com>
2025-03-22 17:54:06 +0200

    Create Remote class which is the same as Remote class in shapter 2, but Encapsule the action of automatically close the door after open. Cause this action is not related to remote, we have to add it in DogDoor class cause it's the responsilbe for opening and closing the doors.

---
+++ third/remote.py
@@ -0,0 +1,17 @@
+from dog_door import DogDoor
+import threading
+class Remote:
+    def __init__(self, door: DogDoor):
+        self._door = door
+    
+    @property
+    def door(self) -> DogDoor:
+        return self._door
+    
+
+    def press_button(self) -> None:
+        print("Pressing the remote control button...")
+        if self.door.is_open():
+            self.door.close()
+        else:
+            self.door.open()
