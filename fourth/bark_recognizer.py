ebeb3ba9b6e82fe7d77bee8ed5a3abf7740c1df5
Mohammed Shaco <mohammedshaco@gmail.com>
2025-05-02 20:50:26 +0200

    modify BarkRecongnzier class. update recognize method to iterate over allowed_barks inside of DogDoog class and use delegation in equals() to check if any of the allowed_barks match with the passed bark

--- fourth/bark_recognizer.py
+++ fourth/bark_recognizer.py
@@ -1,4 +1,5 @@
 from dog_door import DogDoor
+from bark import Bark
 class BarkRecognizer:
     def __init__(self, door: DogDoor) -> None:
         self._door = door
@@ -8,6 +9,11 @@ class BarkRecognizer:
         return self._door
     
     
-    def recognize(self, bark: str) -> None:
-        print(f"BarkRecognizer: Heard a {bark}")
-        self.door.open()
+    def recognize(self, bark: Bark) -> None:
+        print(f"BarkRecognizer: Heard a {bark.sound}")
+        allowed_barks: list[Bark] = self.door.allowed_barks
+        for allowed_bark in allowed_barks:
+            if allowed_bark.equals(bark):
+                self.door.open()
+                return
+        print("This dog is not allowed")
